281731fc0a9f38f05de2c455927e81ba9abc221b
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-09-12 03:12:55 +0200

    - tweaks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ do
     echo "Deploying to: ${ip}..."
     scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@${ip}:~/.docker/config.json
     scp -o StrictHostKeyChecking=no docker-compose.yml pi@${ip}:/home/pi/apps/iot-media-emitter/docker-compose.yml
-    ssh -o StrictHostKeyChecking=no pi@${ip} "echo `hostname` > /home/pi/apps/iot-media-emitter/hostname"
+    ssh -o StrictHostKeyChecking=no pi@${ip} "echo \`hostname\` > /home/pi/apps/iot-media-emitter/hostname"
     ssh -o StrictHostKeyChecking=no pi@${ip} "cd /home/pi/apps/iot-media-emitter && docker-compose pull && docker-compose up -d"
     echo "Deploying to: ${ip}... DONE"
 done
