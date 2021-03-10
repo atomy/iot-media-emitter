46d43be71dbac6641800be7fb6be729a6910bb5c
Marcel Lamm <marcel.lamm@jackinpoint.net>
2021-03-10 22:01:48 +0100

    - ipv6 all the way

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ ips=( "2003:d4:1f23:ba00:5ff:4195:12d:ae12" "2003:d4:1f23:ba00:f4d0:9c00:206f:69
 for ip in "${ips[@]}"
 do
     echo "Deploying to: ${ip}..."
-    scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@$[{ip}]:~/.docker/config.json
-    scp -o StrictHostKeyChecking=no docker-compose.yml pi@$[{ip}]:/home/pi/apps/iot-media-emitter/docker-compose.yml
+    scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@[${ip}]:~/.docker/config.json
+    scp -o StrictHostKeyChecking=no docker-compose.yml pi@[${ip}]:/home/pi/apps/iot-media-emitter/docker-compose.yml
     ssh -o StrictHostKeyChecking=no pi@${ip} "echo \`hostname\` > /home/pi/apps/iot-media-emitter/hostname"
     ssh -o StrictHostKeyChecking=no pi@${ip} "cd /home/pi/apps/iot-media-emitter && docker-compose pull && docker-compose up -d"
     echo "Deploying to: ${ip}... DONE"
