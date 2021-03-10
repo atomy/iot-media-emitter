f340ca6acbc1c8fe35b1ab52418001859e6d1ee8
Marcel Lamm <marcel.lamm@jackinpoint.net>
2021-03-10 23:15:16 +0100

    - ipv6 sucks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,13 @@ then
   exit 1
 fi
 
-ips=( "2003:d4:1f23:ba00:5ff:4195:12d:ae12" "2003:d4:1f23:ba00:f4d0:9c00:206f:69fa" "2003:d4:1f23:ba00:2427:f7a3:3c10:c4ad" )
+ips=( "192.168.2.20" "192.168.2.21" "192.168.2.22" )
 
 for ip in "${ips[@]}"
 do
     echo "Deploying to: ${ip}..."
-    scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@[${ip}]:~/.docker/config.json
-    scp -o StrictHostKeyChecking=no docker-compose.yml pi@[${ip}]:/home/pi/apps/iot-media-emitter/docker-compose.yml
+    scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@${ip}:~/.docker/config.json
+    scp -o StrictHostKeyChecking=no docker-compose.yml pi@${ip}:/home/pi/apps/iot-media-emitter/docker-compose.yml
     ssh -o StrictHostKeyChecking=no pi@${ip} "echo \`hostname\` > /home/pi/apps/iot-media-emitter/hostname"
     ssh -o StrictHostKeyChecking=no pi@${ip} "cd /home/pi/apps/iot-media-emitter && docker-compose pull && docker-compose up -d"
     echo "Deploying to: ${ip}... DONE"
